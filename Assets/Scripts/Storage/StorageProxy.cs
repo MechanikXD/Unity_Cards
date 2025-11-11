a5e21f8ba4c56731f5d2644a2a2f51ab4a138045
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-11-11 10:58:36 +0200

    Storage Proxy

---
+++ Assets/Scripts/Storage/StorageProxy.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Storage
+{
+    public static class StorageProxy
+    {
+        private const string SETTINGS_KEY = "Settings";
+        private static Dictionary<string, object> _settings;
+        private static bool _settingsLoaded;
+
+        public static T GetSetting<T>(string key)
+        {
+            if (!_settingsLoaded) LoadSettings();
+            return (T)_settings![key];
+        }
+
+        public static void SetSetting<T>(string key, T value)
+        {
+            if (!_settingsLoaded) LoadSettings();
+            if (!_settings.TryAdd(key, value))
+            {
+                _settings[key] = value;
+            }
+        }
+
+        private readonly static Dictionary<Type, Action<string, object>> Setters = new Dictionary<Type, Action<string, object>>
+        {
+            [typeof(float)] = (key, num) => PlayerPrefs.SetFloat(key, (float)num),
+            [typeof(string)] = (key, str) => PlayerPrefs.SetString(key, (string)str),
+            [typeof(int)] = (key, num) => PlayerPrefs.SetInt(key, (int)num)
+        };
+        private readonly static Dictionary<Type, Func<string, object>> Getters = new Dictionary<Type, Func<string, object>>
+        {
+            [typeof(float)] = key => PlayerPrefs.GetFloat(key),
+            [typeof(string)] = PlayerPrefs.GetString,
+            [typeof(int)] = key => PlayerPrefs.GetInt(key)
+        };
+        
+        public static T Get<T>(string key)
+        {
+            if (PlayerPrefs.HasKey(key)) return (T)Getters[typeof(T)](key);
+
+            Debug.LogError("PlayerPrefs does not contain key: " + key);
+            return default;
+        }
+
+        public static void Set<T>(string key, T value)
+        {
+            Setters[typeof(T)](key, value);
+            PlayerPrefs.Save();
+        }
+        
+        private static void LoadSettings()
+        {
+            var serialized = PlayerPrefs.GetString(SETTINGS_KEY);
+            _settings = JsonUtility.FromJson<SerializableDictionary<string, object>>(serialized).ToDictionary();
+            _settingsLoaded = true;
+        }
+
+        public static void SaveSettings()
+        {
+            var serializable = new SerializableDictionary<string, object>(_settings);
+            var serialized = JsonUtility.ToJson(serializable);
+            PlayerPrefs.SetString(SETTINGS_KEY, serialized);
+        }
+        
+        public static void Delete(string key) => PlayerPrefs.DeleteKey(key);
+        public static void Clear() => PlayerPrefs.DeleteAll();
+    }
+}
